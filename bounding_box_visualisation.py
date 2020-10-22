dea3479d14d1ea8dd2f0306a1414f4f3e80e3cc6
hobogalaxy <zalewski.ukas@gmail.com>
2020-10-22 22:18:19 +0200

    add dataset visualisation script

---
+++ bounding_box_visualisation.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib import patches
+
+
+path = "data/yolo_dataset/train_set.txt"
+
+with open(path, "r") as file:
+    img_path = file.readline().replace("\n", "")
+    txt_path = img_path.rsplit('.', 1)[0] + '.txt'
+
+with open(txt_path, "r") as file:
+    bboxes = file.readlines()
+
+
+print(img_path)
+print(txt_path)
+
+bounding_boxes = []
+for bbox in bboxes:
+    bbox_data = bbox.replace("\n", "").split(" ")
+    bbox_data = [float(n) for n in bbox_data]
+    bounding_boxes.append(bbox_data)
+    print(bbox_data)
+
+
+fig, ax = plt.subplots()
+fig.set_size_inches(15, 10)
+img = mpimg.imread(img_path)
+ax.imshow(img)
+
+for bbox in bounding_boxes:
+    rect = patches.Rectangle(
+        (bbox[1], bbox[2]),
+        bbox[3],
+        bbox[4],
+        linewidth=2,
+        edgecolor='r',
+        facecolor='none'
+    )
+    ax.add_patch(rect)
+
+plt.show()
+
+fig.savefig("bbox_example.png")
